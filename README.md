386b4a7db035ecb03e81a101adc3243cb1c31408
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-02-15 13:49:10 +0500

    sse-ws-1

---
+++ README.md
@@ -0,0 +1,1 @@
+![CI](https://github.com/NastyaKatyushkina/sse-ws-1/actions/workflows/web.yml/badge.svg)
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Widget from './Widget.js';
+
+const widget = new Widget('wss://ahj-sse-ws-chat.herokuapp.com/ws');
+
+widget.createForm();
